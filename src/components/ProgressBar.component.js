7af03f77a71d0d2fa4a1c5407595935749fbe831
Fernando Treviño Compean <fernandocompean@Fernandos-MacBook-Pro.local>
2020-07-01 18:08:26 -0500

    [ADD]: shape and font styles

--- src/components/ProgressBar.component.js
+++ src/components/ProgressBar.component.js
@@ -4,25 +4,26 @@ const ProgressBar = (props) => {
   const { increment } = props;
 
   const containerStyles = {
-    height: 30,
+    height: 20,
     width: '100%',
     backgroundColor: "#e0e0de",
-    margin: 40,
-    itemAlign: 'center',
+    borderRadius: 50
   }
 
   const fillerStyles = {
     height: '100%',
     width: `${increment}%`,
     backgroundColor: 'magenta',
-    transition: 'width 1s ease-in-out',
+    borderRadius: 'inherit',
     textAlign: 'right',
+    transition: 'width 1s ease-in-out',
   }
 
   const labelStyles = {
-    padding: 10,
+    padding: 5,
     color: 'white',
     fontWeight: 'bold',
+    fontFamily: 'verdana'
   }
 
   return (
