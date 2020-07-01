f2bb2d0c35c6753656d3febc3c0d30794283928f
Fernando Treviño Compean <fernandocompean@Fernandos-MacBook-Pro.local>
2020-07-01 17:43:39 -0500

    [ADD]: Set State & Timer Function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ProgressBar from "./components/ProgressBar.component";
 
 function App() {
+  const [increment, setIncrement] = useState(0);
+  let progress = 0;
+
+  const updateProgressBar = () =>{
+    progress += increment + 10;
+    if(progress <= 100){
+      setIncrement(progress);
+      setTimeout(updateProgressBar, 1000);
+    }
+  }
+
+  useEffect(updateProgressBar, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProgressBar increment={increment} />
     </div>
   );
+
+
 }
 
 export default App;
